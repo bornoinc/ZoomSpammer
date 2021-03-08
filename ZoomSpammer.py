5427f9df7b219dc9b6a67bca434a8e2baeb9c913
ARIF <68831776+mdarifnoor@users.noreply.github.com>
2021-03-08 17:03:27 +0600

    Update ZoomSpammer.py

--- ZoomSpammer.py
+++ ZoomSpammer.py
@@ -1,31 +1,32 @@
-#this program is to spam in Zoom chatbox
-
-
+#this program is to spam in chatbox
+ 
+ 
 import pyautogui as pg 
-
-
-def texter(x, y, n, text):     #(x,y) = position, n= number of hit
+import keyboard
+ 
+ 
+def texter(x, y, n, text, t):     #(x,y) = position, n= number of hit
     #pg.click(x, y)
     for i in range(n):
         pg.click(x, y)
         pg.write(text)
-
-        pg.press('Enter', interval = 0 )
+ 
+        pg.press('Enter', interval = t )
         #pg.click(0,0)/////// #if enter to sent not works 
-
-
-
+ 
+ 
+ 
 while(True):
-    text = input("Enter the text: ")
-    n = input("Enter Number of hit : ")
-    t = input("time delay in second: ")
-    (x,y) = pg.position()
-    
-
-    texter(x, y, int(n), text)
-
-
-
-
-
-
+    print ("Put Your curson on chat box and press 'tab'")
+ 
+    if keyboard.read_key() == "tab":
+        print("You pressed p")
+        (x,y) = pg.position()
+        print (x , y)
+        text = input("Enter the text: ")
+        n = input("Enter Number of hit : ")
+        t = input("time delay in second: ")
+ 
+ 
+ 
+        texter(x, y, int(n), text, t)
